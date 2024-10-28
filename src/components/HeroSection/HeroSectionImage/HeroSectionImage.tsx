1c59a67490b5b95642f6cd060dd30735d61b39d6
Pikablok <benjamincornet62130@gmail.com>
2024-10-28 22:48:22 +0100

    :white_check_mark: Fixes #5, Fixes #6 - Add the text and the image to the heroSection

--- src/components/HeroSection/HeroSectionImage/HeroSectionImage.tsx
+++ src/components/HeroSection/HeroSectionImage/HeroSectionImage.tsx
@@ -3,7 +3,7 @@ import imageCV from './../../../assets/imageCV.png'
 const HeroSectionImage = () => {
   return (
     <div>
-      <img src={imageCV} alt="hero" className="w-124 h-124 object-cover rounded-50 drop-shadow-img shadow-text cursor-pointer" />
+      <img src={imageCV} alt="hero" className="w-124 h-124 object-cover rounded-50 drop-shadow-img shadow-text cursor-pointer brightness-95" />
     </div>
   )
 }
