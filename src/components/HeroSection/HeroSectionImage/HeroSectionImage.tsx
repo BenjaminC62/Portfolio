ab3e87fa5110312ff2dd559a6de42a6e1ac81f4e
Pikablok <benjamincornet62130@gmail.com>
2024-10-29 02:32:46 +0100

    :memo: Fixes #12 - add pulse animation on the image

--- src/components/HeroSection/HeroSectionImage/HeroSectionImage.tsx
+++ src/components/HeroSection/HeroSectionImage/HeroSectionImage.tsx
@@ -1,9 +1,10 @@
 import imageCV from './../../../assets/imageCV.png'
+import './HerSectionImage.css'
 
 const HeroSectionImage = () => {
   return (
     <div>
-      <img src={imageCV} alt="hero" className="w-124 h-124 object-cover rounded-50 drop-shadow-img shadow-text cursor-pointer brightness-95" />
+      <img src={imageCV} alt="hero" className="w-124 h-124 object-cover rounded-50 drop-shadow-img shadow-text cursor-pointer brightness-95 pulse-animation" />
     </div>
   )
 }
