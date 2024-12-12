161ae769342b2343c2d20f33f2ef957eddd4671c
BenjaminC62 <benjamincornet62130@gmail.com>
2024-12-12 23:27:19 +0100

    Ajout de changement

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -1,7 +1,10 @@
+import HeroSectionImage from "../HeroSection/HeroSectionImage/HeroSectionImage.tsx";
+
 export const AboutSection = () => {
     return (
-        <section className="bg-white w-20 h-20 flex mx-auto">
-
+        <section className="flex justify-between mx-auto mt-40">
+            <HeroSectionImage/>
+            <div></div>
         </section>
     );
 };
