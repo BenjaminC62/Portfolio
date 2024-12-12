f301d01954675f953c5f48bcddce207868323898
BenjaminC62 <benjamincornet62130@gmail.com>
2024-12-12 20:09:12 +0100

    Changement du README

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -5,13 +5,19 @@
  */
 const AboutMe = () => {
   return (
-    <div className="bg-gray-100 p-8 rounded-lg shadow-md max-w-lg mx-auto mt-40">
-      <h1 className="text-3xl font-bold text-gray-800 text-center mb-4">About Me</h1>
-      <h2 className="text-xl font-semibold text-gray-600 text-center mb-2">Hello! I'm Cornet Benjamin</h2>
-      <p className="text-gray-700 text-justify leading-relaxed">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus, non architecto vero animi vitae repellat laudantium explicabo, atque hic tempore autem totam doloremque odit nisi.
-      </p>
+    <div className="flex">
+      <div className="bg-gray-100 p-8 rounded-lg shadow-md max-w-lg mx-auto mt-40">
+        <h1 className="text-3xl font-bold text-gray-800 text-center mb-4">About Me</h1>
+        <h2 className="text-xl font-semibold text-gray-600 text-center mb-2">Hello! I'm Cornet Benjamin</h2>
+        <p className="text-gray-700 text-justify leading-relaxed">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Repellendus, non architecto vero animi vitae repellat laudantium explicabo, atque hic tempore autem totam doloremque odit nisi.
+        </p>
+      </div>
+      <div className="mt-40">
+        <p className="text-blue-50">Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, ipsum?</p>
+      </div>
     </div>
+    
   );
 }
 
