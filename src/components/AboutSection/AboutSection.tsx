3d37d59077eeaf0e3b5c781e29bc72a74e113e2d
BenjaminC62 <benjamincornet62130@gmail.com>
2024-12-12 20:38:37 +0100

    :building_construction: #18 - ajout de la section about me

---
+++ src/components/AboutSection/AboutSection.tsx
@@ -0,0 +1,9 @@
+export const AboutSection = () => {
+    return (
+        <section className="bg-white w-20 h-20 flex mx-auto">
+
+        </section>
+    );
+};
+
+export default AboutSection;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,24 +3,26 @@ import Navbar from '../components/Navbar/Navbar'
 import SideNav from '../components/Navbar/SideNav/SideNav';
 import navData from './../components/Navbar/navData.json';
 import ScrollIndicator from '../components/ScrollIndicator/ScrollIndicator';
-import AboutMe from './AboutMe';
+import {AboutSection} from "../components/AboutSection";
 
 
 /**
  *  Home component, this is a functional component which is the home page of the website
- * 
- * @returns 
+ *
+ * @returns
  */
 function Home() {
-  return (
-    <main className="text-white font-poppins">
-      <SideNav data={navData.navList} name={navData.name} lastName={navData.lastName} firstPage={navData.firstPage} homeIcon={navData.homeIcon} />
-      <Navbar data={navData.navList} name={navData.name} lastName={navData.lastName} firstPage={navData.firstPage} homeIcon={navData.homeIcon}/>
-      <HeroSection />
-      <ScrollIndicator />
-      <AboutMe />
-    </main>
-  )
+    return (
+        <main className="text-white font-poppins">
+            <SideNav data={navData.navList} name={navData.name} lastName={navData.lastName}
+                     firstPage={navData.firstPage} homeIcon={navData.homeIcon}/>
+            <Navbar data={navData.navList} name={navData.name} lastName={navData.lastName} firstPage={navData.firstPage}
+                    homeIcon={navData.homeIcon}/>
+            <HeroSection/>
+            <ScrollIndicator/>
+            <AboutSection/>
+        </main>
+    )
 }
 
 export default Home;
