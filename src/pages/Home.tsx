136537511bddc82dfc0211785e4decd83eb458d0
BenjaminC62 <benjamincornet62130@gmail.com>
2024-12-22 21:54:56 +0100

    :art: #23 - ajout de l'animation de scrolling

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,7 @@ import navData from './../components/Navbar/navData.json';
 import ScrollIndicator from '../components/ScrollIndicator/ScrollIndicator';
 import {AboutSection} from "../components/AboutSection";
 import Footer from "../components/Footer/Footer.tsx";
-
+import './../styles/animScroll.css'
 
 /**
  *  Home component, this is a functional component which is the home page of the website
@@ -15,17 +15,20 @@ import Footer from "../components/Footer/Footer.tsx";
 function Home() {
     return (
         <>
-            <header>
-                <SideNav data={navData.navList} name={navData.name} lastName={navData.lastName}
-                         firstPage={navData.firstPage} homeIcon={navData.homeIcon}/>
-                <Navbar data={navData.navList} name={navData.name} lastName={navData.lastName}
-                        firstPage={navData.firstPage}
-                        homeIcon={navData.homeIcon}/>
-            </header>
             <main className="text-white font-poppins">
-                <HeroSection/>
+                <section className="scroll-section">
+                    <SideNav data={navData.navList} name={navData.name} lastName={navData.lastName}
+                             firstPage={navData.firstPage} homeIcon={navData.homeIcon}/>
+                    <Navbar data={navData.navList} name={navData.name} lastName={navData.lastName}
+                            firstPage={navData.firstPage}
+                            homeIcon={navData.homeIcon}/>
+                    <HeroSection/>
+                </section>
                 <ScrollIndicator/>
-                <AboutSection/>
+                <section className="scroll-section">
+                    <AboutSection/>
+                </section>
+                {/* Classic scroll */}
             </main>
             <footer>
                 <Footer/>
