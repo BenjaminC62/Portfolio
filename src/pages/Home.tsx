5b28ea7d1362700d345e6805c478310305c1c62c
BenjaminC62 <benjamincornet62130@gmail.com>
2024-12-24 18:40:35 +0100

    correction du scroll snap pour la nouvelle section

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,31 +1,36 @@
+import {useEffect, useRef} from 'react';
 import HeroSection from '../components/HeroSection/HeroSection';
-import Navbar from '../components/Navbar/Navbar'
+import Navbar from '../components/Navbar/Navbar';
 import SideNav from '../components/Navbar/SideNav/SideNav';
 import navData from './../components/Navbar/navData.json';
 import Footer from "../components/Footer/Footer.tsx";
 import ScrollIndicator from "../components/ScrollIndicator/ScrollIndicator.tsx";
-import {useEffect} from "react";
 import SkillsSection from "../components/SkillsSection/SkillsSection.tsx";
 
-/**
- *  Home component, this is a functional component which is the home page of the website
- *
- * @returns
- */
 function Home() {
+    const lastScrollTopRef = useRef(0);
+
     useEffect(() => {
         const handleScroll = () => {
             const aboutSection = document.querySelector('.about-section');
+            const skillsSection = document.querySelector('.skills-section');
             const scrollTop = window.scrollY;
-            // @ts-ignore
-            const aboutOffsetTop = aboutSection.offsetTop;
-            // @ts-ignore
-            const aboutHeight = aboutSection.offsetHeight;
 
-            if (scrollTop >= aboutOffsetTop && scrollTop < aboutOffsetTop + aboutHeight) {
-                document.documentElement.classList.add('no-scroll-snap');
+            const aboutOffsetTop = (aboutSection as HTMLElement).offsetTop;
+            const aboutHeight = (aboutSection as HTMLElement).offsetHeight;
+            const skillsOffsetTop = (skillsSection as HTMLElement).offsetTop;
+            const skillsHeight = (skillsSection as HTMLElement).offsetHeight;
+
+            const direction = scrollTop > lastScrollTopRef.current ? 'down' : 'up';
+            lastScrollTopRef.current = scrollTop;
+
+            if ((scrollTop >= aboutOffsetTop && scrollTop < aboutOffsetTop + aboutHeight) ||
+                (scrollTop >= skillsOffsetTop && scrollTop < skillsOffsetTop + skillsHeight)) {
+                document.documentElement.style.scrollSnapType = 'none';
+            } else if (scrollTop < aboutOffsetTop && direction === 'up') {
+                document.documentElement.style.scrollSnapType = 'y mandatory';
             } else {
-                document.documentElement.classList.remove('no-scroll-snap');
+                document.documentElement.style.scrollSnapType = 'y mandatory';
             }
         };
 
@@ -33,34 +38,31 @@ function Home() {
         return () => window.removeEventListener('scroll', handleScroll);
     }, []);
 
-
     return (
         <>
             <header>
                 <SideNav data={navData.navList} name={navData.name} lastName={navData.lastName}
                          firstPage={navData.firstPage} homeIcon={navData.homeIcon}/>
-
             </header>
             <main className="text-white font-poppins">
                 <section className="container mx-auto " id="scroll-section">
-                    {/*This is a hack to make the scroll-snap work*/}
                     <div className="mt-8 text-background invisible">space</div>
                     <Navbar data={navData.navList} name={navData.name} lastName={navData.lastName}
-                            firstPage={navData.firstPage}
-                            homeIcon={navData.homeIcon}/>
+                            firstPage={navData.firstPage} homeIcon={navData.homeIcon}/>
                     <HeroSection/>
                 </section>
                 <ScrollIndicator/>
-                <section className="container mx-auto m-40 about-section" id="scroll-section">
+                <section className="container mx-auto mt-40 about-section" id="scroll-section">
                     <SkillsSection/>
+                </section>
+                <section className="container mx-auto skills-section" id="scroll-section">
                 </section>
             </main>
             <footer>
                 <Footer/>
             </footer>
         </>
-
-    )
+    );
 }
 
 export default Home;
