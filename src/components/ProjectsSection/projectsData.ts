bc6b6405210af142481829cadb6be94f30498955
BenjaminC62 <benjamincornet62130@gmail.com>
2024-12-29 23:35:09 +0100

    :page_facing_up: #33 - ajout de details

--- src/components/ProjectsSection/projectsData.ts
+++ src/components/ProjectsSection/projectsData.ts
@@ -6,24 +6,53 @@ import AmazonJuste from './../../assets/projects-images/Amazon Juste Prix.jpg';
 import LBIRD from './../../assets/projects-images/LBIRD.png';
 import LensJudge from './../../assets/projects-images/LensJudge.png';
 
+/* Additonal Images */
+import adventura1 from './../../assets/projects-images/AdditionalImages/Adventura_1.png';
+import adventura2 from './../../assets/projects-images/AdditionalImages/Adventura_2.png';
+import adventura3 from './../../assets/projects-images/AdditionalImages/Adventura_3.png';
+
+import bomberman1 from './../../assets/projects-images/AdditionalImages/Bomberman_1.png';
+import bomberman2 from './../../assets/projects-images/AdditionalImages/Bomberman_2.png';
+import bomberman3 from './../../assets/projects-images/AdditionalImages/Bomberman_3.png';
+
+import amazon1 from './../../assets/projects-images/AdditionalImages/Amazon_1.png';
+import amazon2 from './../../assets/projects-images/AdditionalImages/Amazon_2.png';
+import amazon3 from './../../assets/projects-images/AdditionalImages/Amazon_3.png';
+
+import lensjudge1 from './../../assets/projects-images/AdditionalImages/LensJudge_1.png';
+
+import lbird1 from './../../assets/projects-images/AdditionalImages/lbird_1.png';
+import lbird2 from './../../assets/projects-images/AdditionalImages/lbird_2.png';
+import lbird3 from './../../assets/projects-images/AdditionalImages/lbird_3.png';
+
+
 export const projects = [
     {
         title: "Adventura",
-        githubLink: "https://github.com/username/Adventura",
-        timeTaken: "3 weeks",
-        intro: "Adventura is an interactive RPG game that challenges players to explore a dynamic world full of quests and mysteries.",
+        githubLink: "https://github.com/BenjaminC62/Adventura",
+        timeTaken: "36 heures",
+        intro: "Adventura est une application web innovante qui permet aux utilisateurs de partager et de découvrir des expériences de voyage en van.",
         additionalImages: [
-            "./../../assets/projects-images/LensJudge.png",
-            "./../../assets/projects-images/LensJudge.png",
-            "./../../assets/projects-images/LensJudge.png",
+            adventura1,
+            adventura2,
+            adventura3,
         ],
-        description: "Description 1",
+        description: "Un carnet de voyage en ligne qui centralise et partage les expériences de voyage en van des utilisateurs. Ce projet avait pour objectif de concevoir un site web fonctionnel en PHP Laravel en moins de 36 heures. Notre équipe, composée de 8 membres, incluait 4 étudiants du département Informatique et 4 étudiants issus du département MMI. ",
         image: adventura,
         button_text: "View Project",
         title: "Adventura",
         link: "/projects/Adventura",
     },
     {
+        title: "Bomberman",
+        githubLink: "https://github.com/BenjaminC62/Bomberman-FX",
+        timeTaken: "",
+        intro: "Bomberman ",
+        additionalImages: [
+            bomberman1,
+            bomberman2,
+            bomberman3,
+        ],
         description: "Description 2",
         image: bomberman,
         button_text: "View Project",
@@ -31,6 +60,15 @@ export const projects = [
         link: "/projects/Bomberman",
     },
     {
+        title: "RoseQuartz-Spa",
+        githubLink: "https://github.com/BenjaminC62/RoseQuartz-Spa",
+        timeTaken: "",
+        intro: "RoseQuartz-Spa ",
+        additionalImages: [
+            "./../../assets/projects-images/LensJudge.png",
+            "./../../assets/projects-images/LensJudge.png",
+            "./../../assets/projects-images/LensJudge.png",
+        ],
         description: "Description 2",
         image: roseSpa,
         button_text: "View Project",
@@ -38,6 +76,15 @@ export const projects = [
         link: "/projects/RoseQuartz-Spa",
     },
     {
+        title: "Amazon-Juste-Prix",
+        githubLink: "https://github.com/BenjaminC62/Amazon-Juste-Prix",
+        timeTaken: "",
+        intro: "RoseQuartz-Spa ",
+        additionalImages: [
+            amazon1,
+            amazon2,
+            amazon3
+        ],
         description: "Description 2",
         image: AmazonJuste,
         button_text: "View Project",
@@ -45,6 +92,15 @@ export const projects = [
         link: "/projects/Amazon-Juste-Prix",
     },
     {
+        title: "L-BIRD",
+        githubLink: "https://github.com/BenjaminC62/Unity-Flappy-bird",
+        timeTaken: "",
+        intro: "L-BIRD ",
+        additionalImages: [
+            lbird1,
+            lbird2,
+            lbird3,
+        ],
         description: "Description 2",
         image: LBIRD,
         button_text: "View Project",
@@ -52,6 +108,13 @@ export const projects = [
         link: "/projects/L-BIRD",
     },
     {
+        title: "LensJudge",
+        githubLink: "https://github.com/BenjaminC62/LensJudge",
+        timeTaken: "",
+        intro: "LensJudge",
+        additionalImages: [
+            lensjudge1
+        ],
         description: "Description 2",
         image: LensJudge,
         button_text: "View Project",
--- src/pages/ProjectDetails.tsx
+++ src/pages/ProjectDetails.tsx
@@ -3,10 +3,20 @@ import {projects} from './../components/ProjectsSection/projectsData.ts';
 import Navbar from "../components/Navbar/Navbar.tsx";
 import navData from "../components/Navbar/navData.json";
 import Footer from "../components/Footer/Footer.tsx";
+import {useState} from "react";
 
 const ProjectDetails = () => {
+    const [selectedImage, setSelectedImage] = useState(null);
     const {title} = useParams<{ title: string }>();
     const project = projects.find((p) => p.title === title);
+
+    const openModal = (image) => {
+        setSelectedImage(image);
+    };
+
+    const closeModal = () => {
+        setSelectedImage(null);
+    };
 
     if (!project) {
         return <div className="text-center text-white">Project not found.</div>;
@@ -20,24 +30,35 @@ const ProjectDetails = () => {
             </header>
             <main className="flex-grow flex flex-col items-center text-white py-16 px-8">
                 <div className="max-w-6xl w-full flex flex-col lg:flex-row items-start gap-12">
-                    {/* Image du projet */}
                     <img
                         src={project.image}
                         alt={project.title}
                         className="w-full lg:w-1/2 h-auto object-cover rounded-lg shadow-lg"
                     />
 
-                    {/* Informations du projet */}
                     <div className="flex flex-col items-start lg:w-1/2">
-                        <h1 className="text-5xl font-bold text-[#00FFEE] mb-4">{project.title}</h1>
-                        <a
-                            href={project.githubLink}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                            className="text-[#00FFEE] underline mb-4"
-                        >
-                            View on GitHub
-                        </a>
+                        <div className="flex items-center mb-4">
+                            <h1 className="text-5xl font-bold text-[#00FFEE]">{project.title}</h1>
+                            <a
+                                href={project.githubLink}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                                className="ml-4"
+                                title="View on GitHub"
+                            >
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    width="24"
+                                    height="24"
+                                    viewBox="0 0 24 24"
+                                    fill="#00FFEE"
+                                    className="hover:opacity-80 transition-opacity duration-300"
+                                >
+                                    <path
+                                        d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+                                </svg>
+                            </a>
+                        </div>
                         <p className="text-gray-400 mb-4">
                             <strong>Time Taken:</strong> {project.timeTaken}
                         </p>
@@ -53,27 +74,44 @@ const ProjectDetails = () => {
                     </div>
                 </div>
 
-                {/* Description détaillée */}
-                <div className="mt-12 max-w-6xl w-full">
+                <div className="mt-12 max-w-6xl w-full bg-[#1a1a1a] p-8 rounded-lg shadow-lg">
                     <h2 className="text-3xl font-bold text-[#00FFEE] mb-6">Project Details</h2>
                     <p className="text-lg text-gray-300 leading-relaxed mb-8">
                         {project.description}
                     </p>
 
-                    {/* Images supplémentaires */}
                     <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                         {project.additionalImages.map((image, index) => (
                             <img
                                 key={index}
                                 src={image}
                                 alt={`Project Detail ${index + 1}`}
-                                className="w-full h-auto object-cover rounded-lg shadow-lg"
+                                className="w-full h-auto object-cover rounded-lg shadow-lg cursor-pointer"
+                                onClick={() => openModal(image)}  // Ouvre la modale au clic
                             />
                         ))}
                     </div>
+
+                    {selectedImage && (
+                        <div className="fixed inset-0 bg-black bg-opacity-75 flex justify-center items-center z-50">
+                            <div
+                                className="relative max-w-screen-lg max-h-screen p-6 bg-[#1a1a1a] rounded-lg shadow-xl border-4 border-[#00FFEE]">
+                                <img
+                                    src={selectedImage}
+                                    alt="Selected"
+                                    className="max-w-full max-h-full object-contain rounded-lg"
+                                />
+                                <button
+                                    onClick={closeModal}
+                                    className="absolute top-[-0.7rem] right-0 text-[#00FFEE] text-4xl font-bold hover:text-white transition-all duration-300"
+                                >
+                                    &times;
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </main>
-
 
             <footer>
                 <Footer/>
