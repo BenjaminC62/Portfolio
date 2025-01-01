646e98cf6386fa89b0d232b5a6fe72af19ab3594
BenjaminC62 <benjamincornet62130@gmail.com>
2025-01-01 17:09:51 +0100

    :memo: Fixes #16, Fixes #21 - fin du footer + ajout de la nav bar responsive

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import {NavbarProps} from '../../types';
 import {Link} from 'react-router-dom';
 import HeroSectionLinks from './LinksNavBar/NavBarLinks';
 import NavButtonCV from './NavButtonCV/NavButtonCV';
 
 const Navbar: React.FC<NavbarProps> = ({name, lastName}) => {
+    const [menuOpen, setMenuOpen] = React.useState(false);
+
+    const menuRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (menuRef.current) {
+            menuRef.current.style.zIndex = '99999';
+        }
+
+        if (menuOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+
+        return () => {
+            document.body.style.overflow = 'auto';
+        };
+    }, [menuOpen]);
+
+
+    useEffect(() => {
+        if (menuRef.current) {
+            menuRef.current.style.zIndex = '99999';
+        }
+    }, []);
+
     return (
-        <nav className="bg-main backdrop-blur">
+        <nav className="bg-main">
             <div className="container mx-auto flex flex-wrap justify-between items-center">
                 <div className="flex items-center">
                     <Link to="/" className="text-white font-bold text-2xl md:text-4xl">
@@ -26,11 +53,11 @@ const Navbar: React.FC<NavbarProps> = ({name, lastName}) => {
                 <div className="md:hidden">
                     <button
                         className="text-white focus:outline-none"
-                        aria-label="Toggle navigation"
+                        onClick={() => setMenuOpen(!menuOpen)}
                     >
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            className="h-6 w-6"
+                            className="h-8 w-8"
                             fill="none"
                             viewBox="0 0 24 24"
                             stroke="currentColor"
@@ -39,9 +66,29 @@ const Navbar: React.FC<NavbarProps> = ({name, lastName}) => {
                                 strokeLinecap="round"
                                 strokeLinejoin="round"
                                 strokeWidth={2}
-                                d="M4 6h16M4 12h16m-7 6h7"
+                                d={menuOpen ? 'M6 18L18 6M6 6l12 12' : 'M4 6h16M4 12h16m-7 6h7'}
                             />
                         </svg>
+                    </button>
+                </div>
+            </div>
+
+            <div
+                className={`fixed top-0 h-[1000px] left-[-80px] bg-main bg-opacity-70 w-96 !z-50 transform md:hidden lg:hidden xl:hidden 2xl:hidden  ${
+                    menuOpen ? 'translate-x-1' : '-translate-x-full'
+                } transition-transform duration-300`} ref={menuRef}
+            >
+                <div
+                    className="flex flex-col my-auto items-center justify-center h-full space-y-8 text-center z-[1000]">
+                    <HeroSectionLinks/>
+
+                    <NavButtonCV/>
+
+                    <button
+                        className="text-white text-lg font-bold"
+                        onClick={() => setMenuOpen(false)}
+                    >
+                        Fermer
                     </button>
                 </div>
             </div>
