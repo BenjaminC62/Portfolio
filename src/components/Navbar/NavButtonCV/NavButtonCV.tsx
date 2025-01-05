35058f537776b731d98764c41744be18e65d4c40
BenjaminC62 <benjamincornet62130@gmail.com>
2025-01-05 14:40:21 +0100

    :white_check_mark: Fixes #39 - mise en place du CV en anglais

--- src/components/Navbar/NavButtonCV/NavButtonCV.tsx
+++ src/components/Navbar/NavButtonCV/NavButtonCV.tsx
@@ -1,8 +1,12 @@
 import {Link} from "react-router-dom";
-import cv from '../../../assets/cv/Cv cornet benjamin Portfolio.pdf';
+import cvFR from '../../../assets/cv/CV Cornet Benjamin.pdf';
+import cvEN from '../../../assets/cv/CV Cornet Benjamin EN.pdf';
 import arrowCV from './../../../assets/icons/up-right-arrow.png';
+import {useTranslation} from "react-i18next";
 
 const NavButtonCV = () => {
+    const {i18n} = useTranslation();
+    const cv = i18n.language === 'en' ? cvEN : cvFR;
     return (
         <div className="relative inline-block font-poppins">
             <Link to={cv} target="_blank" title="CV Cornet Benjamin">
